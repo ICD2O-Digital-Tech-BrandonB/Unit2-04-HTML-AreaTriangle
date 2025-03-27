c45fec94edd6fd9fb1211babce7cf13fbb5f6e8a
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-03-27 14:10:19 +0000

    Area of Triangle JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 BrandonBCode All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: BrandonBCode
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate () {
+  // input
+  let lengthA = parseInt(document.getElementById('lengthsidea-of-triangle').value);
+  let lengthB = parseInt(document.getElementById('lengthsideb-of-triangle').value);
+  let lengthC = parseInt(document.getElementById('lengthsidec-of-triangle').value);
+  let height = parseInt(document.getElementById('height-of-triangle').value);
+  let base = parseInt(document.getElementById('base-of-triangle').value);
+
+  // process
+  let area = base * height / 2;
+  let perimeter = lengthA + lengthB + lengthC;
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
